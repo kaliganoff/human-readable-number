5dbf982675bc83642764bd8461c1e1432f55b068
kaliganoff <kaliganoff@yahoo.com>
2023-08-02 18:41:40 +0300

    feat: Write my own function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,47 @@
 module.exports = function toReadable (number) {
-    var converter = require('number-to-words');
-    npm.load(function(err) {
-        // handle errors
-      
-        // install module ffi
-        npm.commands.install(['number-to-words'], function(er, data) {
-          // log errors or data
-        });
-      
-        npm.on('log', function(message) {
-          // log installation progress
-          console.log(message);
-        });
-      });
-    result = converter.toWords(number).replace('-', ' ');
-    return result;
+    var hundred = 100;
+    var thousand = 1000;
+    var remainder = 0;
+    var remainder100 = 0;
+    var result;
+    var upToTwenty = [
+        'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+    ];
+    
+    var tenthsUpToHundred = [
+        'zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+    ];
+    
+    if (number < 20) {
+    result = upToTwenty[number];
+    }
+    else if (number < 100){
+    remainder = number % 10;
+    result = tenthsUpToHundred[Math.floor(number / 10)];
+    if (remainder) {
+        result = result += ' ' + upToTwenty[remainder];
+    }
+    }
+    else if (number < 1000){
+        remainder100 = number % 100;
+        result = upToTwenty[Math.floor(number / 100)] + ' hundred';
+        if (remainder100) {
+            if (remainder100 < 20)
+            result += ' ' + upToTwenty[remainder100];
+             else if (tenthsUpToHundred[Math.floor(remainder100 / 10)] === 'zero')
+                result += '';
+            else {
+            remainder = number % 10;
+            result += ' ' + tenthsUpToHundred[Math.floor(remainder100 / 10)];
+            if (remainder) {
+                    result = result += ' ' + upToTwenty[remainder];
+            }
+            }
+        }
+    } 
+    else if (number = 1000)
+    result = 'one thousand';
+
+return result;
 }
