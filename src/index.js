3b9dd6271b59783b63567db07f3e59d3e2eb35ab
kaliganoff <kaliganoff@yahoo.com>
2023-08-02 15:35:41 +0300

    fix: Add npm loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 module.exports = function toReadable (number) {
     var converter = require('number-to-words');
+    npm.load(function(err) {
+        // handle errors
+      
+        // install module ffi
+        npm.commands.install(['number-to-words'], function(er, data) {
+          // log errors or data
+        });
+      
+        npm.on('log', function(message) {
+          // log installation progress
+          console.log(message);
+        });
+      });
     result = converter.toWords(number).replace('-', ' ');
     return result;
 }
