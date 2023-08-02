5a174543414fc98547baea308c4dcf6e2f03cbc8
kaliganoff <kaliganoff@yahoo.com>
2023-08-02 15:01:02 +0300

    init: Solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function toReadable (number) {
-  
+    var converter = require('number-to-words');
+    result = converter.toWords(number).replace('-', ' ');
+    return result;
 }
